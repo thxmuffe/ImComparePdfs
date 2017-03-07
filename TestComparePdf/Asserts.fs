bef9f0d0ce87f95570edb39d59d2f3a605f9b634
Paavo <paavo.penttila@yap.fi>
2017-03-07 09:59:08 +0200

    Added tests

---
+++ TestComparePdf/Asserts.fs
@@ -0,0 +1,90 @@
+﻿namespace ComparePdfs
+
+module Assert = 
+
+    open Xunit
+    open System.IO
+    open ComparePdfs
+
+    type Results =
+        |DocumentsEqual
+        |DifferencesFound
+
+    let expectedEqual(docs) =
+        // If document path is different they are expected to have differing content as well
+        (docs |> List.distinct).Length = 1
+
+    module DiffFiles =
+        let expected(docs, expectedPages) =
+            expectedPages
+                |> List.map (fun page -> Comparison.DiffFilePath(docs |> List.map FileInfo , page))
+    
+        let Assert_FileExists(file : FileInfo) =
+            Assert.True file.Exists
+
+        let Assert_Exists(docs : list<string>, expectedPages) = 
+            expected(docs, expectedPages)
+                |> List.map Assert_FileExists
+
+        let DeleteExisting(docs : list<string>, expectedPages) = 
+            expected(docs, expectedPages)
+                |> List.iter (fun f -> if File.Exists(f.FullName) then File.Delete(f.FullName) )
+
+    let AssertDifferencesCount( docs , differences : list<FileInfo>, expectedPages : list<int> ) =
+        DiffFiles.Assert_Exists(docs, expectedPages) |> ignore
+            
+        if docs |> expectedEqual then
+            Assert.True( differences.Length = 0 )
+            DocumentsEqual
+        else
+            Assert.True( differences.Length > 0 )
+            DifferencesFound
+
+    let PdfComparisonWithBothMethods ( docs : list<string>, expectedPages ) =
+        DiffFiles.DeleteExisting(docs, expectedPages)
+        // Test finding first
+        let t1 = System.DateTime.Now
+        let resultFirst = AssertDifferencesCount( 
+            docs,
+            docs
+                |> List.map FileInfo
+                |> Comparison.FirstDifference, 
+            expectedPages)
+        let t1_end = System.DateTime.Now
+
+        DiffFiles.DeleteExisting(docs, expectedPages)
+        // Test parallel implementation of findAny
+        let t2 = System.DateTime.Now
+        let resultsAny = AssertDifferencesCount( 
+            docs,
+            docs
+                |> List.map FileInfo
+                |> Comparison.AnyDifference,
+            expectedPages)
+        let t2_end = System.DateTime.Now
+
+        // If there is only one expected difference both method should return the same result
+        if expectedPages.Length = 1 then
+            Assert.Equal(resultFirst, resultsAny)
+
+        printfn "%s" ("Test duration: { FirstDifference=" + t1_end.Subtract(t1).ToString() + " AnyDifference=" +  t2_end.Subtract(t2).ToString() + "}")
+        [resultFirst; resultsAny]
+
+    let PdfComparison ( docs, expectedPages )   =
+        printfn "%s" "Comparing files:"
+        docs |> List.iter (fun f -> printfn "   %s" f )
+        PdfComparisonWithBothMethods( docs, expectedPages)
+
+module AssertErrorRaised =
+
+    open Xunit
+    open System.IO
+
+    let PdfComparison(docs : list<string>, expectedDiffFile : list<int>) =
+        Assert.True (
+            try
+                Assert.PdfComparison(docs, expectedDiffFile) |> ignore
+                false
+            with
+                // TODO: Match to proper exception, not all
+                | :? _ as ex -> true  )
---
+++ TestComparePdf/Tests.fs
@@ -0,0 +1,115 @@
+﻿namespace ComparePdfs
+
+open Xunit
+
+module Tests = 
+
+    // All these should raise an error
+    [<Fact>]
+    let Test_BadInput_ErrorRaised() =
+        // Bad FilePath given
+        AssertErrorRaised.PdfComparison( ["2-page-doxxxxxxt.pdf"], [])
+
+        // Not any files given
+        AssertErrorRaised.PdfComparison( [], [])
+
+        // Cannot comapre only 1 document
+        AssertErrorRaised.PdfComparison( ["2-page-document.pdf"], [])
+        []
+
+    [<Fact>]
+    let Assert_TwoSmallEqualDocuments() =
+        Assert.PdfComparison( 
+            ["2-page-document.pdf";
+            "2-page-document.pdf" ], 
+            [])
+
+    [<Fact>]
+    let Assert_TwoUnequalLengthDocuments() =
+        Assert.PdfComparison( 
+            ["2-page-document.pdf";
+            "11-page-document_with_text_on_page11.pdf" ], 
+            [2])
+
+    [<Fact>]
+    let Assert_TwoUnequalSmallDocuments() =
+        Assert.PdfComparison( 
+            ["2-page-document.pdf";
+            "2-page-document_without_content_on_2nd_page.pdf" ], 
+            [2])
+
+    [<Fact>]
+    let Assert_MultipleUnequalSmallDocuments() =
+        Assert.PdfComparison( 
+            ["2-page-document.pdf";
+            "2-page-document_without_content_on_2nd_page.pdf";
+            "11-page-document_without_text_on_page11.pdf";
+             "2-page-document.pdf"], 
+            [2])
+
+    [<Fact>]
+    let Assert_TwoUnequalMediumSizeDocuments() =
+        Assert.PdfComparison( 
+            ["11-page-document_with_text_on_page11.pdf";
+            "11-page-document_without_text_on_page11.pdf" ], 
+            [11])
+
+    [<Fact>]
+    let Assert_TwoUnequalLargeDocuments() =
+        Assert.PdfComparison( 
+            ["60-page-document_with_text_on_page40.pdf";
+            "60-page-document_without_text_on_page40.pdf" ], 
+            [40])
+
+    [<Fact>]
+    let Assert_MultipleUnequalLargeDocuments() =
+        Assert.PdfComparison( 
+            ["60-page-document_with_text_on_page40.pdf";
+            "60-page-document_without_text_on_page40.pdf";
+            "60-page-document_with_text_on_page40.pdf";
+            "60-page-document_without_text_on_page40.pdf"  ], 
+            [])
+
+    let Assert_MultipleEqualLargeDocuments() =
+        Assert.PdfComparison( 
+            ["60-page-document_with_text_on_page40.pdf";
+            "60-page-document_with_text_on_page40.pdf";
+            "60-page-document_with_text_on_page40.pdf";
+            "60-page-document_with_text_on_page40.pdf";
+            "60-page-document_with_text_on_page40.pdf";
+            "60-page-document_with_text_on_page40.pdf"  ], 
+            [])
+
+    let printResult result = 
+        let origColor = System.Console.ForegroundColor
+        try
+            result
+                |> List.map (fun res ->
+                match res  with
+                | Assert.DocumentsEqual ->
+                    System.Console.ForegroundColor <- System.ConsoleColor.Green
+                    printfn "%s" "Documents are EQUAL"
+                | Assert.DifferencesFound ->
+                    System.Console.ForegroundColor <- System.ConsoleColor.Yellow
+                    printfn "%s" "Documents are DIFFERING" )
+        finally
+             System.Console.ForegroundColor <- origColor
+        |> ignore
+
+    let allTests = [
+        Test_BadInput_ErrorRaised;
+        Assert_TwoSmallEqualDocuments;
+        Assert_TwoUnequalLengthDocuments;
+        Assert_TwoUnequalSmallDocuments;
+        Assert_MultipleUnequalSmallDocuments;
+        Assert_TwoUnequalMediumSizeDocuments;
+        Assert_TwoUnequalLargeDocuments;
+        Assert_MultipleUnequalLargeDocuments;
+        Assert_MultipleEqualLargeDocuments]
+
+    let RunAll() =
+        
+        printfn "%s" "Starting tests"
+
+        allTests
+            |> List.iter (fun test -> test() |> printResult )
