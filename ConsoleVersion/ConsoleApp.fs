f4aae26e41b8ca76a074441635cec8b84a2c8bde
Paavo <paavo.penttila@yap.fi>
2017-03-07 09:59:29 +0200

    Added console application which can be used as standalone

---
+++ ConsoleVersion/ConsoleApp.fs
@@ -0,0 +1,18 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main argv = 
+
+    printfn "Files to compare: %A" argv
+
+    let files = match argv.Length with
+                // | 1 -> DirectoryInfo(argv.[0]).GetFiles() |> List.concat
+                | _ -> argv |> Seq.toList |> List.map FileInfo
+
+    match files |> ComparePdfs.Comparison.AllDifferences with
+    | [] -> printfn "%s" "Completed!"
+    | differences -> printfn "%i Differences found" differences.Length
+    
+    System.Console.ReadLine() |> ignore
+
+    0 // return an integer exit code
